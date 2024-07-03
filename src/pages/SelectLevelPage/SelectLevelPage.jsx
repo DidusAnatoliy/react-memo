225e08f648dda04e5be8810114786cf7aea8310d
didusanatoliy <bereg.007@mail.ru>
2024-07-03 17:09:41 +0300

    Готово

--- src/pages/SelectLevelPage/SelectLevelPage.jsx
+++ src/pages/SelectLevelPage/SelectLevelPage.jsx
@@ -1,10 +1,20 @@
 import { Link } from "react-router-dom";
 import styles from "./SelectLevelPage.module.css";
+import { EasyModeContext } from "../../utils/contextMode";
+import { useContext } from "react";
 
 export function SelectLevelPage() {
+  const { toggleMode } = useContext(EasyModeContext);
   return (
     <div className={styles.container}>
       <div className={styles.modal}>
+        <div className={styles.checkbox}>
+          <label className={styles.labelCheck}>
+            <input onChange={toggleMode} className={styles.input} type="checkbox" name="checkbox" />
+            <span className={styles.checkboxCustom}></span>
+          </label>
+          <h1 className={styles.text}>Легкий режим(3 жизни)</h1>
+        </div>
         <h1 className={styles.title}>Выбери сложность</h1>
         <ul className={styles.levels}>
           <li className={styles.level}>
